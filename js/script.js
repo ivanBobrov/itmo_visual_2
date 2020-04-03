506fbd7c1dfb8d7923b2a82ef3f359defd7525c5
Bobrov Ivan <id.bobr@gmail.com>
2020-04-03 19:34:32 +0300

    Empty axes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,24 @@
-(function (d3) {
+document.addEventListener("DOMContentLoaded", function () {
     const width = 1000;
     const height = 500;
+    const margin = 40;
     const svg = d3.select('#svg-container')
         .attr('width', width)
         .attr('height', height);
 
-    const xLabel = svg.append('text');
-    xLabel.text("Hello");
+    const xScale = d3.scaleLinear().range(([margin * 2, width - margin]));
+    const yScale = d3.scaleLinear().range([height - margin, margin]);
 
+    xAxis = svg.append('g').attr('transform', `translate(0, ${height - margin})`);
+    yAxis = svg.append('g').attr('transform', `translate(${margin * 2}, 0)`);
 
-    d3.select("body").append("span")
-        .text("Hello, world!");
-})(d3);
+    const xLabel = svg.append('text').attr('transform', `translate(${width / 2}, ${height})`);
+    const yLabel = svg.append('text').attr('transform', `translate(${margin}, ${height / 2}) rotate(-90)`);
+
+    xLabel.text("xLabel");
+    yLabel.text("yLabel");
+
+    xAxis.call(d3.axisBottom(xScale));
+    yAxis.call(d3.axisLeft(yScale));
+
+});
